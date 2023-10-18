6cd58bc3c97c6dcdb0a6bf6c19338cc5be2219fb
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-10-18 11:28:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/RealZenX/Lucy_Premium.git /Lucy_Premium
+  git clone https://github.com/phoenix-monarch/LucyX.git /LucyX
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lucy_Premium
+  git clone $UPSTREAM_REPO /LucyX
 fi
-cd /Lucy_Premium
+cd /LucyX
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py   
